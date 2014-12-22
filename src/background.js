2bf6b1675cda18b7ba0646806892263278aebeca
gilleyj <gilleyj@gmail.com>
2014-12-22 09:45:57 -0500

    Adding of comments to project
    Adding comments so betty can read the code better

--- src/background.js
+++ src/background.js
@@ -1,29 +1,21 @@
 
-chrome.webRequest.onBeforeRequest.addListener(function(details) {
-        var user_name = betty_blocker.get_twitter_user_(details.url);
-        console.log(chrome.extension.getURL("blocked.html"));
-        if(user_name !== false) {
-	        var user_list = betty_blocker.load_users();
-        	if(user_list.indexOf(user_name) > -1 ) {
-        		return {redirectUrl: chrome.extension.getURL("blocked.html") + "?user=" + user_name};
-        	}
-        }
-  	}, {
-        urls: ['*://twitter.com/*']
-	}, ['blocking']);
-
-
+// this is our blocker object
+// TODO: need to get jquery in here and also learn
+// more about how this should work as it seems slow right now
 var betty_blocker = {
 	
+	// our fuction to process a click of the "block user now" button
    	block_user: function() {
+   		// find the currently open tab in the browser window
+   		// need to figure this out for safari
 		chrome.tabs.query({ currentWindow: true, active: true }, function(tabs) {
 			if(typeof tabs !== 'undefined' && typeof tabs[0] !== 'undefined') {
+				// need to figure out how to call self within an anonymous call
+				// thse are inefficient
 				var user_name = betty_blocker.get_twitter_user_(tabs[0].url);
 				var user_list = betty_blocker.load_users();
 				user_list.push(user_name);
 				betty_blocker.save_users(user_list);
-				console.log(user_name);
-				console.log(user_list);
 			}
 		});
 	},
@@ -77,9 +69,35 @@ var betty_blocker = {
 };
 
 
+// Add a listener to the webrequest process that parses the URL BEFORE
+// the browser navigates to the page
+chrome.webRequest.onBeforeRequest.addListener(function(details) {
+	// Attempt to extract the Twitter Username from the URL
+	var user_name = betty_blocker.get_twitter_user_(details.url);
+	// if a username was found then
+	if(user_name !== false) {
+		// load up the stored user list (this should be moved into the better_blocker object?)
+		var user_list = betty_blocker.load_users();
+		// find the username in the list of stored users
+		if(user_list.indexOf(user_name) > -1 ) {
+			// if the username was found, then send the browser to our internal
+			// blocked user page
+			return {redirectUrl: chrome.extension.getURL("blocked.html") + "?user=" + user_name};
+		}
+	}
+}, {
+	// we're currently only interested in twitter URLS
+	urls: ['*://twitter.com/*']
+}, ['blocking']);
+
+// Add a listener for the message from the popup.html that the
+// user clicks to block a currently browsed user
 chrome.runtime.onMessage.addListener(
-    function(request,sender,sendResponse) {
+	// anonymouse function to recieve the listener message
+    function(request, sender, sendResponse) {
+    	// if the type of message is what we're interested in
         if(request.type == "betty_block_user_now") {
+        	// call the block_user portion of the script
             betty_blocker.block_user();
         }
     }
--- src/popup.js
+++ src/popup.js
@@ -1,8 +1,14 @@
 
+// add a listener to the popup html file (this is called from that)
+// that listens to the button click
+// NOTE: Need to figure out how to add jquery to this codebase
 document.addEventListener('DOMContentLoaded', function () {
-  var myEl = document.getElementById('betty_block_block_button');
-  myEl.addEventListener('click', function() {
-    chrome.runtime.sendMessage({type: "betty_block_user_now"});
-  }, false);
+	// find the button by ID
+	var myEl = document.getElementById('betty_block_block_button');
+	// add a listener to that button we found
+	myEl.addEventListener('click', function() {
+		// send a remote message to the backend.js to block a user we are browsed too
+		chrome.runtime.sendMessage({type: "betty_block_user_now"});
+	}, false);
 });
 
